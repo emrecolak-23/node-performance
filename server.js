4c648f2c8eeb6017bd1609275f13174c67e4e8d3
Emre ÇOLAK <colakkemre@gmail.com>
2023-04-28 23:55:28 +0300

    node cluster mode and in action

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 const express = require('express');
+const cluster = require('cluster');
 
 const app = express();
 
+// Extreme worst case blocking situations
 function delay(duration) {
   const startTime = Date.now();
-  while (Date.now() - startTime < duration) {}
+  while (Date.now() - startTime < duration) {
+    // event loop is blocked
+  }
 }
 
 app.get('/', (req, res) => {
-  res.send('Performance example');
+  // Following function or built in apis blocked Event loop
+  // JSON.stringify({}) => ""
+  // JSON.parse({})
+  // [1,2,3,4,5].sort()
+  // crypto.pbkdf2
+
+  res.send(`Performance example ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
   delay(9000);
-  res.send('Hello hello');
+  res.send(`Hello hello ${process.pid}`);
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on 3000');
-});
+console.log('Running server.js...');
+if (cluster.isMaster) {
+  console.log('Master has been started...');
+  cluster.fork();
+  cluster.fork();
+} else {
+  console.log('Worker process started...');
+  app.listen(3000);
+}
